9ee1e7bbbd245eaebceed12f839cd7fc169f6c14
Marlon Leon <ghostveene@gmail.com>
2023-03-03 12:15:08 -0500

    added another property

--- src/index.ts
+++ src/index.ts
@@ -78,6 +78,19 @@ const properties: Property[]  = [
         },
         contact: [ +1123495082908, 'andyluger@aol.com'],
         isAvailable: true
+    },
+    {
+        image: './images/london-property.jpg',
+        title: 'Malia Hotel',
+        price: 35,
+        location: {
+            firstLine: 'Room 4',
+            city: 'Malia',
+            code: 45334,
+            country: 'Malaysia',
+        },
+        contact: [ +60349822083, 'lee34@gmail.com'],
+        isAvailable: false
     }
 ]
 
