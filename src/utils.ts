5bc9e4b7f3b76b518bf4eb6c598698e7c8c20dce
Marlon Leon <ghostveene@gmail.com>
2023-03-03 10:25:50 -0500

    fixed totalreviews loyaltyuser paramater

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,11 @@
+import { LoyaltyUser } from "./enums"
+
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
 const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
 const userNameDisplay = document.querySelector('#user') as HTMLElement
 
-export function totalReviews(value: number, reviewer: string, isLoyalUser : boolean) {
-    const iconDisplay = isLoyalUser ? "⭐" : ""
+export function totalReviews(value: number, reviewer: string, isLoyalUser: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? "⭐" : ""
 
     reviewTotalDisplay.innerHTML = `
             review total ${value.toString()}| last reviewed by ${reviewer}${iconDisplay}
