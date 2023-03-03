3475afe8a0ece8308b5a2aaa120cea6dfcbaf115
Marlon Leon <ghostveene@gmail.com>
2023-03-03 10:25:27 -0500

    fixed permissions enum and loyalty user enum

--- src/enums.ts
+++ src/enums.ts
@@ -1,4 +1,10 @@
 export enum Permissions {
-    ADMIN,
-    READ_ONLY
+    ADMIN = "ADMIN",
+    READ_ONLY = "READ_ONLY"
+}
+
+export enum LoyaltyUser {
+    GOLD_USER = "GOLD_USER",
+    SILVER_USER = "SILVER_USER",
+    BRONZE_USER = "BRONZE_USER"
 }
